4de1c7b035f27e214ba75c977d0bdc5af750a833
Darshana <vs_darshana@yahoo.com>
2020-09-15 16:45:42 +0530

    small modification

--- alias.sh
+++ alias.sh
@@ -8,4 +8,4 @@ echo "To start with committing to your git repository, enter <git_com>"
 alias git_com='chmod +x git_commit.sh;./git_commit.sh'
 
 alias auto_com='crontab -e
-1 * * * * cd $USER/$repo && git add . && git commit -m "auto_commit" && git push origin master'
+* * * * * cd $USER/$repo && git add . && git commit -m "auto_commit" && git push origin master'
